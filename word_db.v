2019100d75e3fb4b2dfc6b1d1c8a0f56ab20ded7
Jacob Zhang <jacob.b.zhang@gmail.com>
2022-03-01 16:28:07 -0800

    Update word_db.v

--- word_db.v
+++ word_db.v
@@ -1,3 +1,207 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:30:38 03/19/2013 
+// Design Name: 
+// Module Name:    vga648'h480 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module vga640x480(
+	input wire dclk,			//pixel clock: 25MHz
+	input wire clr,			//asynchronous reset
+	output wire hsync,		//horizontal sync out
+	output wire vsync,		//vertical sync out
+	output reg [2:0] red,	//red vga output
+	output reg [2:0] green, //green vga output
+	output reg [1:0] blue	//blue vga output
+	);
+
+// video structure constants
+parameter hpixels = 800;// horizontal pixels per line
+parameter vlines = 521; // vertical lines per frame
+parameter hpulse = 96; 	// hsync pulse length
+parameter vpulse = 2; 	// vsync pulse length
+parameter hbp = 144; 	// end of horizontal back porch
+parameter hfp = 784; 	// beginning of horizontal front porch
+parameter vbp = 31; 		// end of vertical back porch
+parameter vfp = 511; 	// beginning of vertical front porch
+// active horizontal video is therefore: 784 - 144 = 640
+// active vertical video is therefore: 511 - 31 = 480
+
+// registers for storing the horizontal & vertical counters
+reg [9:0] hc;
+reg [9:0] vc;
+
+// alphabet bitmaps
+// ARTWORK
+localparam ALPHABET [0:25][0:7][0:7] = {
+	 { 8'h0C, 8'h1E, 8'h33, 8'h33, 8'h3F, 8'h33, 8'h33, 8'h00},   // U+0041 (A)
+    { 8'h3F, 8'h66, 8'h66, 8'h3E, 8'h66, 8'h66, 8'h3F, 8'h00},   // U+0042 (B)
+    { 8'h3C, 8'h66, 8'h03, 8'h03, 8'h03, 8'h66, 8'h3C, 8'h00},   // U+0043 (C)
+    { 8'h1F, 8'h36, 8'h66, 8'h66, 8'h66, 8'h36, 8'h1F, 8'h00},   // U+0044 (D)
+    { 8'h7F, 8'h46, 8'h16, 8'h1E, 8'h16, 8'h46, 8'h7F, 8'h00},   // U+0045 (E)
+    { 8'h7F, 8'h46, 8'h16, 8'h1E, 8'h16, 8'h06, 8'h0F, 8'h00},   // U+0046 (F)
+    { 8'h3C, 8'h66, 8'h03, 8'h03, 8'h73, 8'h66, 8'h7C, 8'h00},   // U+0047 (G)
+    { 8'h33, 8'h33, 8'h33, 8'h3F, 8'h33, 8'h33, 8'h33, 8'h00},   // U+0048 (H)
+    { 8'h1E, 8'h0C, 8'h0C, 8'h0C, 8'h0C, 8'h0C, 8'h1E, 8'h00},   // U+0049 (I)
+    { 8'h78, 8'h30, 8'h30, 8'h30, 8'h33, 8'h33, 8'h1E, 8'h00},   // U+004A (J)
+    { 8'h67, 8'h66, 8'h36, 8'h1E, 8'h36, 8'h66, 8'h67, 8'h00},   // U+004B (K)
+    { 8'h0F, 8'h06, 8'h06, 8'h06, 8'h46, 8'h66, 8'h7F, 8'h00},   // U+004C (L)
+    { 8'h63, 8'h77, 8'h7F, 8'h7F, 8'h6B, 8'h63, 8'h63, 8'h00},   // U+004D (M)
+    { 8'h63, 8'h67, 8'h6F, 8'h7B, 8'h73, 8'h63, 8'h63, 8'h00},   // U+004E (N)
+    { 8'h1C, 8'h36, 8'h63, 8'h63, 8'h63, 8'h36, 8'h1C, 8'h00},   // U+004F (O)
+    { 8'h3F, 8'h66, 8'h66, 8'h3E, 8'h06, 8'h06, 8'h0F, 8'h00},   // U+0050 (P)
+    { 8'h1E, 8'h33, 8'h33, 8'h33, 8'h3B, 8'h1E, 8'h38, 8'h00},   // U+0051 (Q)
+    { 8'h3F, 8'h66, 8'h66, 8'h3E, 8'h36, 8'h66, 8'h67, 8'h00},   // U+0052 (R)
+    { 8'h1E, 8'h33, 8'h07, 8'h0E, 8'h38, 8'h33, 8'h1E, 8'h00},   // U+0053 (S)
+    { 8'h3F, 8'h2D, 8'h0C, 8'h0C, 8'h0C, 8'h0C, 8'h1E, 8'h00},   // U+0054 (T)
+    { 8'h33, 8'h33, 8'h33, 8'h33, 8'h33, 8'h33, 8'h3F, 8'h00},   // U+0055 (U)
+    { 8'h33, 8'h33, 8'h33, 8'h33, 8'h33, 8'h1E, 8'h0C, 8'h00},   // U+0056 (V)
+    { 8'h63, 8'h63, 8'h63, 8'h6B, 8'h7F, 8'h77, 8'h63, 8'h00},   // U+0057 (W)
+    { 8'h63, 8'h63, 8'h36, 8'h1C, 8'h1C, 8'h36, 8'h63, 8'h00},   // U+0058 (X)
+    { 8'h33, 8'h33, 8'h33, 8'h1E, 8'h0C, 8'h0C, 8'h1E, 8'h00},   // U+0059 (Y)
+    { 8'h7F, 8'h63, 8'h31, 8'h18, 8'h4C, 8'h66, 8'h7F, 8'h00}   // U+005A (Z)
+};
+
+ reg [31:0] ctr;
+ always @(posedge dclk) begin
+ ctr <= ctr + 1;
+end
+
+wire in_db;
+
+word_db Word_db(.word(ctr), .in_db(in_db));
+
+// Horizontal & vertical counters --
+// this is how we keep track of where we are on the screen.
+// ------------------------
+// Sequential "always block", which is a block that is
+// only triggered on signal transitions or "edges".
+// posedge = rising edge  &  negedge = falling edge
+// Assignment statements can only be used on type "reg" and need to be of the "non-blocking" type: <=
+always @(posedge dclk or posedge clr)
+begin
+	// reset condition
+	if (clr == 1)
+	begin
+		hc <= 0;
+		vc <= 0;
+	end
+	else
+	begin
+		// keep counting until the end of the line
+		if (hc < hpixels - 1)
+			hc <= hc + 1;
+		else
+		// When we hit the end of the line, reset the horizontal
+		// counter and increment the vertical counter.
+		// If vertical counter is at the end of the frame, then
+		// reset that one too.
+		begin
+			hc <= 0;
+			if (vc < vlines - 1)
+				vc <= vc + 1;
+			else
+				vc <= 0;
+		end
+		
+	end
+end
+
+wire box_i;
+assign box_i = (vc - vbp) / 80;
+
+wire box_j;
+assign box_j = (hc - hbp - 120) / 80;
+
+wire [2:0] ltr_x;
+assign ltr_x = ((vc - vbp + 72) % 80) / 8;
+wire [2:0] ltr_y;
+assign ltr_y = ((hc - hbp + 32) % 80) / 8;
+
+wire v_in_box;
+assign v_in_box = ((vc - vbp + 72) % 80) < 64;
+
+wire h_in_box;
+assign h_in_box = ((hc - hbp + 32) % 80) < 64;
+
+
+// generate sync pulses (active low)
+// ----------------
+// "assign" statements are a quick way to
+// give values to variables of type: wire
+assign hsync = (hc < hpulse) ? 0:1;
+assign vsync = (vc < vpulse) ? 0:1;
+
+always @(*) begin
+// first check if we're within vertical active video range
+	if (vc >= vbp && vc < vfp)
+	begin
+		// main display area
+		if (hc >= (hbp) && hc < (hbp+640))
+		begin
+			// main game area
+			if (hc >= (hbp+120) && hc < (hbp+520)) begin
+				if (h_in_box && v_in_box) begin
+					if (ALPHABET[box_i + box_j][ltr_x][ltr_y]) begin
+						red = 0;
+						green = 0;
+						blue = 0;
+					end else begin
+						red = 3'b111;
+						green = 3'b111;
+						blue = 2'b11;
+					end
+				end else begin
+					red = 0;
+					green = 0;
+					blue = 0;
+				end
+			end
+			// Ukraine flag
+			else if (vc >= (vbp + 240)) begin
+				red = 3'b111;
+				green = 3'b111;
+				blue = 0;
+			end else begin
+				red = 0;
+				green = 0;
+				blue = 2'b11;
+			end
+		end
+		// we're outside active horizontal range so display black
+		else
+		begin
+			red = 0;
+			green = 0;
+			blue = 0;
+		end
+	end
+	// we're outside active vertical range so display black
+	else
+	begin
+		red = 0;
+		green = 0;
+		blue = 0;
+	end
+end
+
+endmodule
+
+
+
 //Bloom filter storing 5757 words in 32768 bits with 4 hash functions
 localparam [14:0] hash_params [0:3] [0:4] = {
 {15'h3aba, 15'h5b55, 15'h66ba, 15'h4f75, 15'h1093},
